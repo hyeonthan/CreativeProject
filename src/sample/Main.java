07f929986c6aa96b12218eaeb49169c877df17c4
carpem_sarah <hsr1203@naver.com>
2021-05-15 16:11:31 +0900

    Main commit

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("Destination.fxml"));
-        primaryStage.setTitle("여정시");
+        primaryStage.setTitle("여정시(여행정보시스템)");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
