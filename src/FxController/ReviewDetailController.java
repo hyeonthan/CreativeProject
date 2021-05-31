b1f1ab9177f546dae96d11b43547509e7a0c85e7
realrealbback <taek4549@gmail.com>
2021-05-31 19:28:49 +0900

    수정

--- src/FxController/ReviewDetailController.java
+++ src/FxController/ReviewDetailController.java
@@ -1,9 +1,13 @@
 package FxController;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
 
 import DTO.ReviewDTO;
 import javafx.fxml.FXML;
@@ -28,17 +32,21 @@ public class ReviewDetailController implements Initializable {
 		this.saveReviewDTO = saveReviewDTO;
 		tDestinationName.setText(saveReviewDTO.getDestination_name());
 		tUserId.setText(saveReviewDTO.getUser_id());
+		tScope.setText(Integer.toString(saveReviewDTO.getScope()));
+		tReportingDate.setText(saveReviewDTO.getReporting_date().toString());
+		String modifyDate = "";
+		if(saveReviewDTO.getModify_date() != null){
+			modifyDate = saveReviewDTO.getModify_date().toString();
+		}
+		tModifyDate.setText(modifyDate);
+		taContent.setText(saveReviewDTO.getContent());
+		byte[] image = saveReviewDTO.getImage();
+		ByteArrayInputStream bis = new ByteArrayInputStream(image);
+		Image img = new Image(bis);	
+		iv_reviewImage.setImage(img);
 	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		try {
-			String path = "C:\\Users\\jyw\\Desktop\\gwang.jpg";
-			FileInputStream fis = new FileInputStream(path);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			Image img = new Image(bis);	
-			iv_reviewImage.setImage(img);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+	
 	}
 }
