6926767bcd3a75b644b6fac6debf0c7d483d9669
carpem_sarah <hsr1203@naver.com>
2021-06-02 16:51:04 +0900

    메인창 중앙으로 띄우기

--- src/FxController/Main.java
+++ src/FxController/Main.java
@@ -85,6 +85,7 @@ public class Main extends Application {
 
 				Stage primaryStage = (Stage) btn_login.getScene().getWindow();
 				primaryStage.setScene(new Scene(root));
+				primaryStage.centerOnScreen(); //중앙으로 창 띄우기
 				primaryStage.show();
 			} catch (Exception e) {
 				e.printStackTrace();
