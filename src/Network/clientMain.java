169c35cf69f87bb6929ef95d54ca2d335d235eb1
tjdehd3698 <khdkhd12@gmail.com>
2021-06-03 16:39:55 +0900

    수정

--- src/Network/clientMain.java
+++ src/Network/clientMain.java
@@ -24,7 +24,7 @@ public class clientMain extends Application {
 	public static void main(String[] args) {
 		try {
 			String localHostAddress = InetAddress.getLocalHost().getHostAddress(); // ip 주소
-			Socket socket = new Socket("192.168.237.50", 5000);
+			Socket socket = new Socket("192.168.225.151", 5000);
 			
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
